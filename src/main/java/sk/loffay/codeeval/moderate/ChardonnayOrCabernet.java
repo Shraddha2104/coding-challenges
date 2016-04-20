8962d649fd11ccf879f42eb9fcc1960b644cd208
Pavol Loffay <ploffay@redhat.com>
2016-04-20 22:57:47 +0200

    chardonnay or cabernet

---
+++ src/main/java/sk/loffay/codeeval/moderate/ChardonnayOrCabernet.java
@@ -0,0 +1,79 @@
+package sk.loffay.codeeval.moderate;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Pavol Loffay
+ */
+public class ChardonnayOrCabernet {
+
+    public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+
+            Params params = parseLine(line);
+            List<String> matchedWines = matchedWines(params);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String wine: matchedWines) {
+                stringBuilder.append(wine).append(" ");
+            }
+            System.out.println(stringBuilder.toString().trim());
+        }
+    }
+
+    public static List<String> matchedWines(Params params) {
+
+        List<String> result = new ArrayList<>();
+
+        for (String wine: params.wines) {
+            String wineToModify = wine.toLowerCase();
+
+            boolean containsAllLetters = true;
+            for (char chOfLetters: params.letters.toCharArray()) {
+
+                if (wineToModify.indexOf(chOfLetters) >= 0) {
+                    wineToModify = wineToModify.replaceFirst(""+chOfLetters, "1");
+                } else {
+                    containsAllLetters = false;
+                    break;
+                }
+            }
+            if (containsAllLetters) {
+                result.add(wine);
+            }
+        }
+
+        if (result.isEmpty()) {
+            result.add("False");
+        }
+        return result;
+    }
+
+    private static Params parseLine(String line) {
+
+        String[] split = line.split("\\|");
+        String letters = split[1].trim().toLowerCase();
+        String[] wines = split[0].trim().split(" ");
+
+        return new Params(letters, wines);
+    }
+
+    public static class Params {
+        String letters;
+        String[] wines;
+
+        public Params(String letters, String[] wines) {
+            this.letters = letters;
+            this.wines = wines;
+        }
+    }
+}
